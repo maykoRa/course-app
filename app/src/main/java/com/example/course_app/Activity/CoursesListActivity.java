9c98cdb598e4c83ebc461688bc8a171233b9de75
andi muh haikal lukman <113303662+itsdccal@users.noreply.github.com>
2024-05-03 23:41:12 +0800

    update datadummy

--- app/src/main/java/com/example/course_app/Activity/CoursesListActivity.java
+++ app/src/main/java/com/example/course_app/Activity/CoursesListActivity.java
@@ -43,31 +43,31 @@ public class CoursesListActivity extends AppCompatActivity {
     private void initRecyclerView() {
         ArrayList<CoursesDomain> items = new ArrayList<>();
         items.add(new CoursesDomain("Advanced certification program in AI",1000000 , R.drawable.ic_1));
-        items.add(new CoursesDomain("Google Cloud Platform Architecture", 2000000, R.drawable.ic_2));
+        items.add(new CoursesDomain("Google Cloud Platform Architecture", 2000000, R.drawable.gc_1));
         items.add(new CoursesDomain("Fundamental of Java Programming", 3000000, R.drawable.ic_3));
-        items.add(new CoursesDomain("Introduction to UI design history", 4000000, R.drawable.ic_4));
-        items.add(new CoursesDomain("PG Program in Big Data Engineering", 5000000, R.drawable.ic_5));
+        items.add(new CoursesDomain("Introduction to UI design history", 4000000, R.drawable.f));
+        items.add(new CoursesDomain("PG Program in Big Data Engineering", 5000000, R.drawable.bd_1));
 
         ArrayList<CoursesDomain> items2 = new ArrayList<>();
-        items2.add(new CoursesDomain("Advanced certification program in AI 2",1000000 , R.drawable.ic_1));
-        items2.add(new CoursesDomain("Google Cloud Platform Architecture", 2000000, R.drawable.ic_2));
-        items2.add(new CoursesDomain("Fundamental of Java Programming", 3000000, R.drawable.ic_3));
-        items2.add(new CoursesDomain("Introduction to UI design history", 4000000, R.drawable.ic_4));
-        items2.add(new CoursesDomain("PG Program in Big Data Engineering", 5000000, R.drawable.ic_5));
+        items2.add(new CoursesDomain("User Experience Design Fundamentals", 1500000, R.drawable.ux));
+        items2.add(new CoursesDomain("UI Prototyping with Adobe XD", 1800000, R.drawable.adobe));
+        items2.add(new CoursesDomain("Interaction Design Principles", 2000000, R.drawable.f));
+        items2.add(new CoursesDomain("Responsive Web Design", 2200000, R.drawable.f));
+        items2.add(new CoursesDomain("Mobile App Design", 2500000, R.drawable.as));
 
         ArrayList<CoursesDomain> items3 = new ArrayList<>();
-        items3.add(new CoursesDomain("Advanced certification program in AI 3",1000000 , R.drawable.ic_1));
-        items3.add(new CoursesDomain("Google Cloud Platform Architecture", 2000000, R.drawable.ic_2));
-        items3.add(new CoursesDomain("Fundamental of Java Programming", 3000000, R.drawable.ic_3));
-        items3.add(new CoursesDomain("Introduction to UI design history", 4000000, R.drawable.ic_4));
-        items3.add(new CoursesDomain("PG Program in Big Data Engineering", 5000000, R.drawable.ic_5));
+        items3.add(new CoursesDomain("Introduction to Artificial Intelligence", 1200000, R.drawable.ic_1));
+        items3.add(new CoursesDomain("Machine Learning Foundations", 1500000, R.drawable.ml));
+        items3.add(new CoursesDomain("Deep Learning Basics", 1800000, R.drawable.ml));
+        items3.add(new CoursesDomain("Natural Language Processing", 2000000, R.drawable.ml));
+        items3.add(new CoursesDomain("Computer Vision", 2200000, R.drawable.ai));
 
         ArrayList<CoursesDomain> items4 = new ArrayList<>();
-        items4.add(new CoursesDomain("Advanced certification program in AI 4",1000000 , R.drawable.ic_1));
-        items4.add(new CoursesDomain("Google Cloud Platform Architecture", 2000000, R.drawable.ic_2));
-        items4.add(new CoursesDomain("Fundamental of Java Programming", 3000000, R.drawable.ic_3));
-        items4.add(new CoursesDomain("Introduction to UI design history", 4000000, R.drawable.ic_4));
-        items4.add(new CoursesDomain("PG Program in Big Data Engineering", 5000000, R.drawable.ic_5));
+        items4.add(new CoursesDomain("Introduction to Information Technology", 1000000, R.drawable.it));
+        items4.add(new CoursesDomain("Computer Networks Fundamentals", 1500000, R.drawable.cn));
+        items4.add(new CoursesDomain("Database Management Systems", 1800000, R.drawable.database));
+        items4.add(new CoursesDomain("Cybersecurity Essentials", 2000000, R.drawable.cs));
+        items4.add(new CoursesDomain("IT Project Management", 2200000, R.drawable.itp));
 
         recyclerViewCourse=findViewById(R.id.view);
         recyclerViewCourse.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL,false));
